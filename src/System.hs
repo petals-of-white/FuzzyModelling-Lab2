f76a633028caf9f37ffc164e7e98c3cbfa33ecfb
Maksym Syvash <maxim.sivash@gmail.com>
2024-12-14 16:27:20 +0200

    added rules

--- src/System.hs
+++ src/System.hs
@@ -1,17 +1,28 @@
 module System where
-import           Huzzy.Base.Sets    hiding (FSet (is))
-import           Inputs
-
+import           Huzzy.Base.Sets      hiding (FSet (is))
 import           Huzzy.Base.Systems
-import qualified Inputs.Illuminance as Illuminance
-import           Inputs.RoadType    as RoadType
-import           OptimalSpeed             
-
-apply :: MF a -> a -> Double
-apply (MF f) = f
+import           Inputs
+import qualified Inputs.Illuminance   as Illuminance
+import qualified Inputs.Precipitation as Precipation
+import qualified Inputs.RoadSlope     as RoadSlope
+import qualified Inputs.RoadSurface   as RoadSurface
+import           Inputs.RoadType      as RoadType
+import qualified Inputs.Traffic       as Traffic
+import qualified Inputs.Visibility    as Visibility
+import           OptimalSpeed
 
 is :: a -> MF a -> Double
 is e (MF f) = f e
+
+
+ruleBase :: Fuzzy a => (a -> a -> a) -> [a] -> a
+ruleBase = foldr1
+
+accumulate :: Fuzzy a => [a] -> a
+accumulate = ruleBase (?||)
+
+fuzzyOptimalSpeed :: CrispInput -> MF OptimalSpeed
+fuzzyOptimalSpeed = accumulate . makeRules
 
 makeRules :: CrispInput -> [MF OptimalSpeed]
 makeRules CrispInput {
@@ -22,7 +33,60 @@ makeRules CrispInput {
     traffic,
     precipationRate,
     roadSlope
-} = 
+} =
     [
-        -- (roadType `is` RoadType.highway ?&& roadSurface `is` Illuminance.ok) `weight` 0.6 =|> OptimalSpeed.high
+        (roadType `is` RoadType.pedestrianZone) =*> OptimalSpeed.veryLow,
+
+        (roadType `is` RoadType.settlement ?&& maxVisibility `is` Visibility.low) =*> OptimalSpeed.veryLow,
+
+        (roadType `is` RoadType.settlement ?&& traffic `is` Traffic.high) =*> OptimalSpeed.veryLow,
+
+        (roadType `is` RoadType.settlement ?&&
+            roadSurface `is` RoadSurface.slippery ?&& maxVisibility `is` Visibility.low) =*> OptimalSpeed.veryLow,
+
+        (roadType `is` RoadType.settlement ?&& precipationRate `is` Precipation.heavy
+            ?&& illuminance `is` Illuminance.bad) =*> OptimalSpeed.veryLow,
+
+
+        (roadType `is` RoadType.settlement ?&& roadSurface `is` RoadSurface.normal
+            ?&& roadSlope `is` RoadSlope.flat) =*> OptimalSpeed.low,
+
+        (roadType `is` RoadType.outOfSettlement ?&& roadSlope `is` RoadSlope.steepDescent
+        ?&& (roadSurface `is` RoadSurface.slippery
+                ?|| (illuminance  `is` Illuminance.bad ?&& maxVisibility `is` Visibility.low)))
+                =*> OptimalSpeed.veryLow,
+
+
+        (roadType `is` RoadType.outOfSettlement ?&& illuminance `is` Illuminance.ok
+        ?&& precipationRate `is` Precipation.moderate ?&& maxVisibility `is` Visibility.moderate)
+            =*> OptimalSpeed.low,
+
+        (roadType `is` RoadType.outOfSettlement ?&& precipationRate `is` Precipation.light
+            ?&& traffic `is` Traffic.moderate) =*> OptimalSpeed.moderate,
+
+        (roadType `is` RoadType.outOfSettlement ?&& roadSurface `is` RoadSurface.normal) =*> OptimalSpeed.high,
+
+
+        (roadType `is` RoadType.highway ?&& roadSlope `is` RoadSlope.steepDescent
+
+        ?&& (precipationRate `is` Precipation.heavy ?|| traffic `is` Traffic.high)) =*> OptimalSpeed.veryLow,
+
+
+        (roadType `is` RoadType.highway ?&& roadSlope `is` RoadSlope.smoothDescent
+        ?&& roadSurface `is` RoadSurface.slippery
+        ?&& maxVisibility `is` Visibility.low) =*> OptimalSpeed.low,
+
+
+
+        (roadType `is` RoadType.highway ?&& roadSlope `is` RoadSlope.steepAscent
+            ?&& roadSurface `is` RoadSurface.slippery) =*> OptimalSpeed.moderate,
+
+        (roadType `is` RoadType.highway
+        ?&& illuminance `is` Illuminance.ok ?&& precipationRate `is` Precipation.light) =*> OptimalSpeed.high,
+
+
+
+        (roadType `is` RoadType.highway ?&& precipationRate `is` Precipation.no
+        ?&& roadSlope `is` RoadSlope.flat ?&& roadSurface `is` RoadSurface.normal
+        ?&& maxVisibility `is` Visibility.high) =*> OptimalSpeed.veryHigh
     ]
